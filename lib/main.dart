9c2bd4f31c532ee8e1bad6f7d8e8588dd810dc3d
Akshay Kumar <akshaymain95@gmail.com>
2023-05-22 21:16:06 +0530

    BlocConsumer implemented

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<CounterCubit, CounterState>(
+      body: BlocConsumer<CounterCubit, CounterState>(
         listener: (context, state) {
           if (state.counter == 3) {
             showDialog(
@@ -54,16 +54,40 @@ class _MyHomePageState extends State<MyHomePage> {
             }));
           }
         },
-        child: BlocBuilder<CounterCubit, CounterState>(
-          builder: (context, state) {
-            return Center(
-                child: Text(
-              '${state.counter}',
-              style: const TextStyle(fontSize: 52.0),
-            ));
-          },
-        ),
+        builder: (context, state) {
+          return Center(
+              child: Text(
+            '${state.counter}',
+            style: const TextStyle(fontSize: 52.0),
+          ));
+        },
       ),
+      // body: BlocListener<CounterCubit, CounterState>(
+      //   listener: (context, state) {
+      //     if (state.counter == 3) {
+      //       showDialog(
+      //           context: context,
+      //           builder: (context) {
+      //             return AlertDialog(
+      //               content: Text('counter is ${state.counter}'),
+      //             );
+      //           });
+      //     } else if (state.counter == -1) {
+      //       Navigator.push(context, MaterialPageRoute(builder: (context) {
+      //         return const OtherPage();
+      //       }));
+      //     }
+      //   },
+      //   child: BlocBuilder<CounterCubit, CounterState>(
+      //     builder: (context, state) {
+      //       return Center(
+      //           child: Text(
+      //         '${state.counter}',
+      //         style: const TextStyle(fontSize: 52.0),
+      //       ));
+      //     },
+      //   ),
+      // ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
