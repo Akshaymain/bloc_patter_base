fbfa76d69a268a51df01c78139dca7d1e67066a5
Akshay Kumar <akshaymain95@gmail.com>
2023-05-26 08:39:01 +0530

    Cubit to cubit communication using blocListener

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -8,34 +8,40 @@ import 'package:equatable/equatable.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  int incrementSize = 1;
-  final ColorCubit colorCubit;
-  late final StreamSubscription colorSubscription;
-
-  CounterCubit({required this.colorCubit}) : super(CounterState.inital()) {
-    colorSubscription = colorCubit.stream.listen((ColorState colorState) {
-      if (colorState.color == Colors.red) {
-        incrementSize = 1;
-      } else if (colorState.color == Colors.green) {
-        incrementSize = 10;
-      } else if (colorState.color == Colors.blue) {
-        incrementSize = 100;
-      } else if (colorState.color == Colors.black) {
-        emit(state.copyWith(counter: state.counter - 100));
-        incrementSize = -100;
-      }
-    });
+  CounterCubit() : super(CounterState.inital());
+  void changeCounter({required int incrementSize}) {
+    emit(CounterState(counter: state.counter + incrementSize));
   }
 
-  void changeCounter() {
-    emit(state.copyWith(counter: state.counter + incrementSize));
-  }
+  /// --- Cubit to cubit communication using stream subscription -- ///
+  // int incrementSize = 1;
+  // final ColorCubit colorCubit;
+  // late final StreamSubscription colorSubscription;
 
-  @override
-  Future<void> close() {
-    colorSubscription.cancel();
-    return super.close();
-  }
+  // CounterCubit({required this.colorCubit}) : super(CounterState.inital()) {
+  //   colorSubscription = colorCubit.stream.listen((ColorState colorState) {
+  //     if (colorState.color == Colors.red) {
+  //       incrementSize = 1;
+  //     } else if (colorState.color == Colors.green) {
+  //       incrementSize = 10;
+  //     } else if (colorState.color == Colors.blue) {
+  //       incrementSize = 100;
+  //     } else if (colorState.color == Colors.black) {
+  //       emit(state.copyWith(counter: state.counter - 100));
+  //       incrementSize = -100;
+  //     }
+  //   });
+  // }
+
+  // void changeCounter() {
+  //   emit(state.copyWith(counter: state.counter + incrementSize));
+  // }
+
+  // @override
+  // Future<void> close() {
+  //   colorSubscription.cancel();
+  //   return super.close();
+  // }
 
   // -- Increment Decrement counter -- //
   // void increment() {
--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,7 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => ColorCubit()),
-        BlocProvider(
-            create: (context) =>
-                CounterCubit(colorCubit: context.read<ColorCubit>())),
+        BlocProvider(create: (context) => CounterCubit()),
       ],
       child: MaterialApp(
         title: 'Cubit to Cubit',
@@ -34,45 +32,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int incrementSize = 1;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.watch<ColorCubit>().state.color,
-      appBar: AppBar(title: const Text('Cubit2Cbuit')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  context.read<ColorCubit>().changeColor();
-                },
-                child: const Text(
-                  'Change color',
-                  style: TextStyle(fontSize: 24.0),
-                )),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              '${context.watch<CounterCubit>().state.counter}',
-              style: const TextStyle(
-                  fontSize: 52.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  context.read<CounterCubit>().changeCounter();
-                },
-                child: const Text(
-                  'Increment Counter',
-                  style: TextStyle(fontSize: 24.0),
-                ))
-          ],
+    return BlocListener<ColorCubit, ColorState>(
+      listener: (context, state) {
+        if (state.color == Colors.red) {
+          incrementSize = 1;
+        } else if (state.color == Colors.green) {
+          incrementSize = 10;
+        } else if (state.color == Colors.blue) {
+          incrementSize = 100;
+        } else if (state.color == Colors.black) {
+          context.read<CounterCubit>().changeCounter(incrementSize: -100);
+          incrementSize = -100;
+        }
+      },
+      child: Scaffold(
+        backgroundColor: context.watch<ColorCubit>().state.color,
+        appBar: AppBar(title: const Text('Cubit2Cbuit')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    context.read<ColorCubit>().changeColor();
+                  },
+                  child: const Text(
+                    'Change color',
+                    style: TextStyle(fontSize: 24.0),
+                  )),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                '${context.watch<CounterCubit>().state.counter}',
+                style: const TextStyle(
+                    fontSize: 52.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    context
+                        .read<CounterCubit>()
+                        .changeCounter(incrementSize: incrementSize);
+                  },
+                  child: const Text(
+                    'Increment Counter',
+                    style: TextStyle(fontSize: 24.0),
+                  ))
+            ],
+          ),
         ),
       ),
     );
